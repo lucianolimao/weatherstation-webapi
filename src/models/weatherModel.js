d97eb135da312fc397de4e5310f6915be7f4e563
Luciano Lima <nanabakid.lima@gmail.com>
2020-01-12 22:39:10 -0300

    criação da api para coleta de dados da estação meteorologica

---
+++ src/models/weatherModel.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose')
+
+const weatherModelSchema = new mongoose.Schema({
+        "createdDate": {
+          "type": "Date",
+          "default": Date.now
+        },
+        "station": {
+          "mac": {
+            "type": "String"
+          }
+        },
+        "coord": {
+          "lon": {
+            "type": "Number"
+          },
+          "lat": {
+            "type": "Number"
+          }
+        },
+        "weather": {
+          "temperature": {
+            "value": {
+              "type": "Number"
+            },
+            "unit": {
+              "type": "String"
+            }
+          },
+          "humidity": {
+            "value": {
+              "type": "Number"
+            },
+            "unit": {
+              "type": "String"
+            }
+          },
+          "pressure": {
+            "value": {
+              "type": "Number"
+            },
+            "unit": {
+              "type": "String"
+            }
+          },
+          "wind": {
+            "value": {
+              "type": "Number"
+            },
+            "unit": {
+              "type": "String"
+            }
+          },
+          "direction": {
+            "value": {
+              "type": "String"
+            }
+          }
+        }
+})
+
+module.exports = mongoose.model('WeatherModel', weatherModelSchema)
---
+++ src/routes/weather.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const router = express.Router()
+const WeatherModel = require('../models/weatherModel')
+
+// Get all weather
+router.get('/', async (req, res) => {
+    try{
+        const weather = await WeatherModel.find()
+        res.json(weather)
+    }catch(err){
+        res.status(500).json({ message: err.message })
+    }
+})
+
+// Get one weather
+router.get('/:id', (req, res) => {
+})
+
+// Create one weather
+router.post('/', async (req, res) => {
+    const weather = WeatherModel({
+        station: {
+            mac: req.body.station.mac
+        },
+        coord: {
+            lon: req.body.coord.lon,
+            lat: req.body.coord.lat,
+        },
+        weather: {
+            temperature: {
+                value: req.body.weather.temperature.value,
+                unit: req.body.weather.temperature.unit
+            },
+            humidity: {
+                value: req.body.weather.humidity.value,
+                unit: req.body.weather.humidity.unit
+            },
+            pressure: {
+                value: req.body.weather.pressure.value,
+                unit: req.body.weather.pressure.unit
+            },
+            wind: {
+                value: req.body.weather.wind.value,
+                unit: req.body.weather.wind.unit
+            },
+            direction: {
+                value: req.body.weather.direction.value
+            }
+        }
+    })
+
+    try {
+    const newWeather = await weather.save()
+    res.status(201).json(newWeather)
+    } catch (err) {
+    res.status(400).json({ message: err.message })
+    }
+})
+
+module.exports = router
